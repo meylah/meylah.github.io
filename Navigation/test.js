cf5bdd023287979bcb94da20f43b9e74b33a5e2d
meylah <heymilaco@gmail.com>
2017-02-10 19:57:35 -0500

    Update test.js

--- Navigation/test.js
+++ Navigation/test.js
@@ -39,6 +39,6 @@ let sensor = new MagnetometerSensor();
 sensor.start();
 
 sensor.onchange = function(event) {
-document.getElementById("hiThere").innerHTML = "MFx: " + event.reading.magneticFieldX + "<br/>" + "MFy: " + event.reading.magneticFieldY;
+document.getElementById("hiThere1").innerHTML = "MFx: " + event.reading.magneticFieldX + "<br/>" + "MFy: " + event.reading.magneticFieldY;
 };
 sensor.onerror = event => console.log(event.error.name, event.error.message);
